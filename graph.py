ae8ad013da95f9b5235a29cd5948653d07eb3d3a
rverdile <ryanverdile@gmail.com>
2021-02-08 14:28:33 -0500

    Graphing works for cpu andgpu

--- graph.py
+++ graph.py
@@ -1,41 +1,76 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-temp_data = pd.read_csv('temp_data.csv') 
-pwr_data = pd.read_csv('pwr_data.csv')
+sys_info = pd.read_csv('sys_info.csv') 
+
+# ================= CPU ================= #
 
 fig, ax1 = plt.subplots()
+plt.title('CPU Temp/Power',fontsize=20)
 
-t_init = temp_data.timestamp[0]
+# Convert timestamps to minutes elapsed
+t_init = sys_info.timestamp[0]
 t = list()
-
-for i in temp_data.timestamp:
+for i in sys_info.timestamp:
 	t.append((i - t_init)/60)
 
-color = 'tab:red'
-ax1.set_xlabel('time (min)')
-ax1.set_ylabel('temp (C)', color=color)
-ax1.plot(t, temp_data.cpu_temp, color=color)
+# ======= Temperature ======= #
+color = 'tab:blue'
+ax1.set_xlabel('Time (min)',fontsize=14)
+ax1.set_ylabel('Temperature (C)', color=color,fontsize=14)
+ax1.set_ylim([0,100])
+ax1.plot(t, sys_info.cpu_temp, color=color) # plot CPU temp
 ax1.tick_params(axis='y', labelcolor=color)
 
+# ======= Power ======= #
 ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
-color = 'tab:blue'
-ax2.set_ylabel('power consumption (W)', color=color)  # we already handled the x-label with ax1
-ax2.plot(t, pwr_data.cpu_pwr, color=color)
+color = 'tab:red'
+ax2.set_ylabel('Power Consumption (W)', color=color,fontsize=14)  # already handled the x-label with ax1
+ax2.set_ylim([0,20])
+ax2.plot(t, sys_info.cpu_pwr, color=color) # plot CPU power
 ax2.tick_params(axis='y', labelcolor=color)
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
-plt.show()
 
 # save the plot as a file
-fig.savefig('cpu_data.jpg',
+fig.savefig('cpu_info.jpg',
             format='jpeg',
             dpi=100,
             bbox_inches='tight')
+            
+# ================= GPU ================= #
 
-# data = pd.read_csv('data.csv')
-# data.cpu_pwr
-# data.cpu_temp
-# data.gpu_pwr
-# data.gpu_temp
+fig, ax1 = plt.subplots()
+plt.title('CPU Temp/Power',fontsize=20)
+
+# Convert timestamps to minutes elapsed
+t_init = sys_info.timestamp[0]
+t = list()
+for i in sys_info.timestamp:
+	t.append((i - t_init)/60)
+
+# ======= Temperature ======= #
+color = 'tab:blue'
+ax1.set_xlabel('Time (min)',fontsize=14)
+ax1.set_ylabel('Temperature (C)', color=color,fontsize=14)
+ax1.set_ylim([0,100])
+ax1.plot(t, sys_info.gpu_temp, color=color) # plot GPU temp
+ax1.tick_params(axis='y', labelcolor=color)
+
+# ======= Power ======= #
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = 'tab:red'
+ax2.set_ylabel('Power Consumption (W)', color=color,fontsize=14)  # we already handled the x-label with ax1
+ax2.set_ylim([0,20])
+ax2.plot(t, sys_info.gpu_pwr, color=color) # plot GPU power
+ax2.tick_params(axis='y', labelcolor=color)
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+
+# save the plot as a file
+fig.savefig('gpu_info.jpg',
+            format='jpeg',
+            dpi=100,
+            bbox_inches='tight')
